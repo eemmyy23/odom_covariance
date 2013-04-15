64aeab26cfd06369e594f4a16e7b743ab862a29a
Emanuel Puscalau <eemmyy23@yahoo.com>
2013-04-15 18:04:14 +0200

    good brodcaster for imu

--- src/imu_tf_broadcaster.cpp
+++ src/imu_tf_broadcaster.cpp
@@ -5,36 +5,38 @@
 
 std::string frameId;
 sensor_msgs::Imu imu;
+nav_msgs::Odometry odom;
 
 void imuCallback(const sensor_msgs::Imu::ConstPtr& msg) {
 	imu.header = msg->header;
 }
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+	odom.pose.pose.position = msg->pose.pose.position;
 
-	tf::TransformBroadcaster br;
-  	tf::Transform transform;
-
-        transform.setOrigin( tf::Vector3(msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z + 0.3) );
-//        transform.setRotation( tf::Quaternion(imu->orientation.x, imu->orientation.y, imu->orientation.z, imu->orientation.w) );
-	transform.setRotation( tf::Quaternion(0, 0, 0) );
-        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), frameId, imu.header.frame_id));
-
-	ROS_INFO("new tf  frame_id=%s->child=%s",frameId.c_str(),imu.header.frame_id.c_str());
 
 }
-
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "imu_tf_broadcaster");
   ros::NodeHandle node;
+
+  tf::TransformBroadcaster br;
+  tf::Transform transform;
 
   frameId = (ros::param::get("~frame_id", frameId)) ? frameId : "/odom";
 
   ros::Subscriber sub1 = node.subscribe("imu/data", 100, imuCallback);
   ros::Subscriber sub2 = node.subscribe("RosAria/pose", 100, odomCallback);
 
-  ros::spin();
 
+  ros::Rate rate(10.0);
+    while (node.ok()){
+      ros::spinOnce();
+      transform.setOrigin( tf::Vector3(odom.pose.pose.position.x, odom.pose.pose.position.y, odom.pose.pose.position.z + 0.3) );
+      transform.setRotation( tf::Quaternion(0, 0, 0) );
+      br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "/odom", "/base_imu"));
+      rate.sleep();
+    }
   return 0;
 }
