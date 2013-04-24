632dae55093c55dc88e7db31a81ea4b439cbf3b6
Emanuel Puscalau <eemmyy23@yahoo.com>
2013-04-25 00:16:59 +0200

    imu imu

--- src/imu_tf_broadcaster.cpp
+++ src/imu_tf_broadcaster.cpp
@@ -2,13 +2,68 @@
 #include <tf/transform_broadcaster.h>
 #include <sensor_msgs/Imu.h>
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Vector3.h>
+
+ros::Publisher rpy_pub;
 
 std::string frameId;
 sensor_msgs::Imu imu;
 nav_msgs::Odometry odom;
+double radToDegree = 57.2957795;
+
+double firstYaw;
+bool init=false;
+int delay = 0;
+
+double limitPi(double a){
+	//constrain to -PI ... PI
+	if (a > 180)
+		a-=180;
+	else if (a <-180)
+		a+=180;
+	return a;
+}
 
 void imuCallback(const sensor_msgs::Imu::ConstPtr& msg) {
 	imu.header = msg->header;
+	// Convert quaternion to RPY.
+	    tf::Quaternion q;
+	    double roll, pitch, yaw;
+	    double limitYaw,deltaYaw;
+	    tf::quaternionMsgToTF(msg->orientation, q);
+	    tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
+
+	    roll*=radToDegree;
+	    pitch*=radToDegree;
+	    yaw=yaw*radToDegree;
+	    //limitYaw = limitPi(yaw);
+//	    ROS_DEBUG("RPY = (%lf, %lf, %lf)", roll, pitch, yaw);
+
+	    //if (delay>0) delay--;
+
+	    if (!init){
+	    	firstYaw = yaw;
+	    	//deltaYaw = firstYaw;
+	    	ROS_INFO("start yaw = %lf",firstYaw);
+	    	init = true;
+	    }
+
+	    //deltaYaw  = limitYaw-firstYaw;
+	    //deltaYaw = yaw - firstYaw;
+	    //if ( !(deltaYaw > -180 && deltaYaw < 180))
+	    //	deltaYaw = yaw - 360 + firstYaw;
+	    deltaYaw = yaw - firstYaw;
+	    if(yaw - firstYaw < 0)
+	    	deltaYaw += 360;
+	    if (deltaYaw > 180)
+	    	deltaYaw -= 360;
+
+	    //ROS_INFO("%lf = %lf - %lf",deltaYaw,yaw,firstYaw);
+	    geometry_msgs::Vector3 rpy;
+	    rpy.x = yaw;
+	    rpy.y = 0;
+	    rpy.z = deltaYaw;
+	    rpy_pub.publish(rpy);
 }
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
@@ -16,6 +71,7 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
 
 
 }
+
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "imu_tf_broadcaster");
@@ -28,6 +84,7 @@ int main(int argc, char** argv){
 
   ros::Subscriber sub1 = node.subscribe("imu/data", 100, imuCallback);
   ros::Subscriber sub2 = node.subscribe("RosAria/pose", 100, odomCallback);
+  rpy_pub = node.advertise<geometry_msgs::Vector3>("imu/rpy", 100);
 
 
   ros::Rate rate(10.0);
